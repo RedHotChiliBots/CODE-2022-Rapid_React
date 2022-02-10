5cdc20a473070ae629940b5177c8339cc2a61ed9
Steve Seifert <sseifert@redhotchilibots.com>
2022-02-09 21:57:48 -0500

    Fix Climber shuffleboard tab

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -10,13 +10,15 @@ import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 import frc.robot.Constants.PneumaticChannelConstants;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.Constants.CANidConstants;
 
 public class Climber extends SubsystemBase {
@@ -24,32 +26,39 @@ public class Climber extends SubsystemBase {
   // Front & Back Climb Solenoids
   public DoubleSolenoid climbRight = null;
   public DoubleSolenoid climbLeft = null;
-  
+
   public CANSparkMax climbRightMotor = null;
   public CANSparkMax climbLeftMotor = null;
 
   // ==============================================================
-	// Identify encoders and PID controllers
+  // Identify encoders and PID controllers
   private RelativeEncoder leftEncoder;
   private RelativeEncoder rightEncoder;
-	
-	private SparkMaxPIDController climbPIDController;
+
+  private SparkMaxPIDController climbPIDController;
+
+  private final ShuffleboardTab climberTab = Shuffleboard.getTab("Climber");
+  private NetworkTableEntry cbLeftPos = climberTab.addPersistent("Left Position", 0.0).getEntry();
+  private NetworkTableEntry cbRightPos = climberTab.addPersistent("Right Position", 0.0).getEntry();
+  private NetworkTableEntry cbSetPoint = climberTab.addPersistent("PIDSetpoint", 0.0).getEntry();
 
   private double setPoint = 0;
 
   public Climber() {
-    
+
     System.out.println("Climber Constructor Starting");
 
     // Initialize climb solenoids
-    climbLeft = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PneumaticChannelConstants.kClimbLeftExtend, PneumaticChannelConstants.kClimbLeftRetract);
-    climbRight = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PneumaticChannelConstants.kClimbRightExtend, PneumaticChannelConstants.kClimbRightRetract);
-    
-		// ==============================================================
-		// Identify left and right Motors
+    climbLeft = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PneumaticChannelConstants.kClimbLeftExtend,
+        PneumaticChannelConstants.kClimbLeftRetract);
+    climbRight = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PneumaticChannelConstants.kClimbRightExtend,
+        PneumaticChannelConstants.kClimbRightRetract);
+
+    // ==============================================================
+    // Identify left and right Motors
 
     climbLeftMotor = new CANSparkMax(CANidConstants.kClimbLeftMotor, MotorType.kBrushless);
-		climbRightMotor = new CANSparkMax(CANidConstants.kClimbRightMotor, MotorType.kBrushless);
+    climbRightMotor = new CANSparkMax(CANidConstants.kClimbRightMotor, MotorType.kBrushless);
 
     climbLeftMotor.restoreFactoryDefaults();
     climbRightMotor.restoreFactoryDefaults();
@@ -58,27 +67,28 @@ public class Climber extends SubsystemBase {
     climbRightMotor.setIdleMode(IdleMode.kBrake);
 
     // Group the left and right motors
-		climbRightMotor.follow(climbLeftMotor, true); // invert direction of right motor
+    climbRightMotor.follow(climbLeftMotor, true); // invert direction of right motor
 
-		// ==============================================================
-		// Identify left and right Encoders
-		
+    // ==============================================================
+    // Identify left and right Encoders
+
     leftEncoder = climbLeftMotor.getEncoder();
     rightEncoder = climbRightMotor.getEncoder();
 
     leftEncoder.setPositionConversionFactor(ClimberConstants.kPosFactorIPC);
-		rightEncoder.setPositionConversionFactor(ClimberConstants.kPosFactorIPC);
+    rightEncoder.setPositionConversionFactor(ClimberConstants.kPosFactorIPC);
 
     // ==============================================================
-		// Identify PID controller
-		climbPIDController = climbLeftMotor.getPIDController();
-		
+    // Identify PID controller
+    climbPIDController = climbLeftMotor.getPIDController();
+
     climbPIDController.setP(ClimberConstants.kP);
     climbPIDController.setI(ClimberConstants.kI);
     climbPIDController.setD(ClimberConstants.kD);
     // climbPIDController.setIZone(ClimberConstants.kIz);
     // climbPIDController.setFF(ClimberConstants.kFF);
-    // climbPIDController.setOutputRange(ClimberConstants.kMinOutput, ClimberConstants.kMaxOutput);
+    // climbPIDController.setOutputRange(ClimberConstants.kMinOutput,
+    // ClimberConstants.kMaxOutput);
 
     System.out.println("Climber Constructor Finished");
   }
@@ -86,10 +96,10 @@ public class Climber extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-		SmartDashboard.putNumber("Left Position",  leftEncoder.getPosition());
-		SmartDashboard.putNumber("Right Position",  rightEncoder.getPosition());
+    cbLeftPos.setDouble(leftEncoder.getPosition());
+    cbRightPos.setDouble(rightEncoder.getPosition());
   }
-  
+
   public void climberSwivel() {
     climbLeft.set(Value.kForward);
     climbRight.set(Value.kForward);
@@ -100,15 +110,14 @@ public class Climber extends SubsystemBase {
     climbRight.set(Value.kReverse);
   }
 
-  public boolean atTarget(){
+  public boolean atTarget() {
     return Math.abs(setPoint - leftEncoder.getPosition()) <= ClimberConstants.kDistanceTolerance;
   }
 
-
   public void climbPosition(double setPoint) {
-		// m_distPIDController.setSetpoint(distance * ChassisConstants.kPosFactor);
+    // m_distPIDController.setSetpoint(distance * ChassisConstants.kPosFactor);
     this.setPoint = setPoint;
-		SmartDashboard.putNumber("PIDSetpoint",  setPoint);
-		climbPIDController.setReference(setPoint, CANSparkMax.ControlType.kPosition);
-	}
+    cbSetPoint.setDouble(setPoint);
+    climbPIDController.setReference(setPoint, CANSparkMax.ControlType.kPosition);
+  }
 }
